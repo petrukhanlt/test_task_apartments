e5ba9a9a927a9cbaf8d167b67844baf9a6fcdf3e
Alexander Petrukha <a.petrukha@newlinetechnologies.net>
2021-04-13 10:33:54 +0300

    created controller, model, route, migration

---
+++ database/migrations/2021_04_13_073053_create_apartments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->index();
+            $table->integer('price')->unsigned();
+            $table->integer('bedrooms')->unsigned();
+            $table->integer('bathrooms')->unsigned();
+            $table->integer('storeys')->unsigned();
+            $table->integer('garages')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartments');
+    }
+}
